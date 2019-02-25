559b56e878c586aece7f13abfdff7e1b232b0f4e
bolgheroni <robertobolgheroni@gmail.com>
2019-02-25 14:04:10 -0300

    Updating translate.friends

--- public/js/translate.friends.js
+++ public/js/translate.friends.js
@@ -4,13 +4,11 @@ function doTranslate(option){
     if(option == 1){
         nav.children[0].textContent = "Humano do Ed"
         nav.children[1].textContent = "Sobre nós"
-        nav.children[2].textContent = "Amigos do Ed"
         translateText[0].style.display = "none"
         translateText[1].style.display = "inline"
     }else{
         nav.children[0].textContent = "Ed's Human"
         nav.children[1].textContent = "About us"
-        nav.children[2].textContent = "Ed's Friends"
         translateText[1].style.display = "none"
         translateText[0].style.display = "inline"
     }
