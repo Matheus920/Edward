b5e24b5ec382e2d076cea227ea15c266b97d44e6
bolgheroni <robertobolgheroni@gmail.com>
2019-02-25 14:21:35 -0300

    Friends page finished and translated

--- public/js/translate.friends.js
+++ public/js/translate.friends.js
@@ -1,14 +1,19 @@
 function doTranslate(option){
     var nav = document.querySelector("nav")
+    var msection = document.querySelector("main section")
     var translateText = document.querySelectorAll("footer a")
     if(option == 1){
         nav.children[0].textContent = "Humano do Ed"
         nav.children[1].textContent = "Sobre nós"
+        msection.children[0].textContent = "Amigos do Edward"
+        msection.children[2].textContent = "Sheldon é o melhor amigo de Edward! Eles se conhecem desde que eram filhotinhos. Os dois gostam de leite quente e um do outro! Por favor, não deixe que esta amizade morra!"
         translateText[0].style.display = "none"
         translateText[1].style.display = "inline"
     }else{
         nav.children[0].textContent = "Ed's Human"
         nav.children[1].textContent = "About us"
+        msection.children[0].textContent = "Edward's Friends"
+        msection.children[2].textContent = "Sheldon is Edward's best friend! They know each other since they were little puppies. They both like warm milk and each other! Please, don't let this friendship die!"
         translateText[1].style.display = "none"
         translateText[0].style.display = "inline"
     }
